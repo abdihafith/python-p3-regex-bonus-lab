292f479bb20c9637647373eb783b87e90b8f551b
Abdihafith <abdyhafith@gmail.com>
2024-06-02 14:36:50 +0300

    done

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,5 @@
 import re
 
-my_pattern = r""
+my_pattern = r"[A-Z][a-z]*'*[a-z]\s[a-z' ]*today'*[ a-z, ]*[?\.]+"
+#my_pattern = r"(?:It's such a lovely day today\.|Some weather we're having today, huh?|Maybe today's just not my day\.)"
 my_regex = re.compile(my_pattern)
-
